180c6cd6095d2b11c60f42fcdb505b8d4fb97480
ZuZKho <matveymag@mail.ru>
2023-09-11 12:10:25 +0700

    test3

--- Task_1_1_2/src/main/java/Polynomial.java
+++ Task_1_1_2/src/main/java/Polynomial.java
@@ -1,34 +1,38 @@
+/**
+ * Класс реализующий представление полинома.
+ */
 public class Polynomial {
 
     private int[] coefs;
     private int n;
 
-    Polynomial (int[] arr) {
+    Polynomial(int[] arr) {
         this.n = arr.length;
         this.coefs = new int[arr.length];
         System.arraycopy(arr, 0, this.coefs, 0, n);
     }
 
-//    private void realloc(int nwN) {
-//        int[] newCoefs = new int[nwN];
-//        System.arraycopy(this.coefs, 0, newCoefs, 0, Math.min(this.n, nwN));
-//        this.coefs = newCoefs;
-//        this.n = nwN;
-//    }
-//
-//    private void normalize() {
-//        int nwN = this.n - 1;
-//        while(nwN > 0 && this.coefs[nwN] == 0) {
-//            nwN--;
-//        }
-//        realloc(nwN + 1);
-//    }
+    private int[] normalize(int[] arr) {
+        int nwN = Math.max(0, arr.length - 1);
+        while (nwN > 0 && arr[nwN] == 0) {
+            nwN--;
+        }
+        int[] buf = new int[nwN + 1];
+        System.arraycopy(arr, 0, buf, 0, nwN + 1);
+        return buf;
+    }
 
+    /**
+     * Сложение двух полиномов.
+     *
+     * @param b второе слагаемое
+     * @return результат сложения
+     */
     public Polynomial plus(Polynomial b) {
         int nwN = Math.max(this.n, b.n);
         int[] newCoefs = new int[nwN];
 
-        for(int i = 0; i < nwN; i++) {
+        for (int i = 0; i < nwN; i++) {
             if (i < this.n) {
                 newCoefs[i] += this.coefs[i];
             }
@@ -37,14 +41,21 @@ public class Polynomial {
             }
         }
 
+        newCoefs = normalize(newCoefs);
         return new Polynomial(newCoefs);
     }
 
+    /**
+     * Вычитание двух полиномов.
+     *
+     * @param b полином - вычитаемое
+     * @return разность полиномов
+     */
     public Polynomial minus(Polynomial b) {
         int nwN = Math.max(this.n, b.n);
         int[] newCoefs = new int[nwN];
 
-        for(int i = 0; i < nwN; i++) {
+        for (int i = 0; i < nwN; i++) {
             if (i < this.n) {
                 newCoefs[i] += this.coefs[i];
             }
@@ -53,68 +64,103 @@ public class Polynomial {
             }
         }
 
+        newCoefs = normalize(newCoefs);
         return new Polynomial(newCoefs);
     }
 
+    /**
+     * Умножение двух полиномов.
+     *
+     * @param b второй множитель
+     * @return произведение полиномов
+     */
     public Polynomial times(Polynomial b) {
         int nwN = this.n + b.n - 1;
         int[] newCoefs = new int[nwN];
-        for(int i = 0; i < this.n; i++) {
-            for(int j = 0; j < b.n; j++) {
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < b.n; j++) {
                 newCoefs[i + j] += this.coefs[i] * b.coefs[j];
             }
         }
+        newCoefs = normalize(newCoefs);
         return new Polynomial(newCoefs);
     }
 
+    /**
+     * Вычисления значения полинома в точке.
+     *
+     * @param x точка, в которой вычисляется значение
+     * @return значение в точке x
+     */
     public int evaluate(int x) {
         int cur = 1;
         int res = 0;
-        for(int coef: coefs) {
+        for (int coef: coefs) {
             res += cur * coef;
             cur *= x;
         }
         return res;
     }
 
+    /**
+     * Взятие производной от полинома.
+     *
+     * @param x порядок производной
+     * @return x-тая производная полинома
+     */
     public Polynomial differentiate(int x) {
 
         int[] newCoefs = new int[this.n];
         int nn = this.n;
         System.arraycopy(this.coefs, 0, newCoefs, 0, this.n);
 
-        for(int i = 0; i < x; i++) {
+        for (int i = 0; i < Math.min(this.n, x); i++) {
             System.arraycopy(newCoefs, 1, newCoefs, 0, nn - 1);
             newCoefs[nn - 1] = 0;
-            for(int j = 0; j < nn - 1; j++) {
+            for (int j = 0; j < nn - 1; j++) {
                 newCoefs[j] *= (j + 1);
             }
             nn--;
         }
 
         // Удалим лишние нули
-        int nwN = nn - 1;
-        while(nwN > 0 && newCoefs[nwN] == 0) {
+        int nwN = Math.max(0, nn - 1);
+        while (nwN > 0 && newCoefs[nwN] == 0) {
             nwN--;
         }
-
         int[] buf = new int[nwN + 1];
         System.arraycopy(newCoefs, 0, buf, 0, nwN + 1);
 
         return new Polynomial(buf);
     }
 
+    /**
+     * Сравнение двух полиномов.
+     *
+     * @param b второй полином
+     * @return равны ли полиномы
+     */
     public boolean isEqual(Polynomial b) {
         if (this.n != b.n) return false;
-        for(int i = 0; i < this.n; i++) {
+        for (int i = 0; i < this.n; i++) {
             if (this.coefs[i] != b.coefs[i]) return false;
         }
         return true;
     }
 
+    /**
+     * Формирует строковое представление полинома.
+     *
+     * @return строковое представление полинома
+     */
     public String toString() {
+
+        if (this.n == 1 && this.coefs[0] == 0) {
+            return "0";
+        }
+
         String res = "";
-        for(int i = n - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             if (this.coefs[i] != 0) {
                 if (res == "") {
                     res += this.coefs[i];
@@ -133,12 +179,5 @@ public class Polynomial {
         return res;
     }
 
-    public static void main(String[] args) {
-        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
-        Polynomial p2 = new Polynomial(new int[] {3, 2, 8});
-        System.out.println("Hello, world!");
-        System.out.println(p1.plus(p2.differentiate(1)).toString());
-        System.out.println(p1.times(p2).evaluate(2));
-    }
-
+    
 }
--- Task_1_1_2/src/test/java/PolynomialTest.java
+++ Task_1_1_2/src/test/java/PolynomialTest.java
@@ -7,28 +7,44 @@ class PolynomialTest {
 
     @Test
     void plus() {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[] {3, 2, 8});
+        Assertions.assertEquals("7x^3 + 6x^2 + 19x + 6",p1.plus(p2.differentiate(1)).toString());
     }
 
     @Test
     void minus() {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[] {3, 2, 8});
+        Assertions.assertTrue(p1.minus(p2).isEqual(new Polynomial(new int[]{1, 1, -2, 7})));
+        Assertions.assertEquals((p1.minus(p1)).toString(), "0");
     }
 
     @Test
     void times() {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        Assertions.assertEquals((p1.times(new Polynomial(new int[]{0}))).toString(), "0");
     }
 
     @Test
     void evaluate() {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[] {3, 2, 8});
+        Assertions.assertEquals(3510, p1.times(p2).evaluate(2));
     }
 
     @Test
     void differentiate() {
+        Assertions.assertTrue((new Polynomial(new int[]{1, 2, 3, 0, 4, 13})).differentiate(0).isEqual(new Polynomial((new int[]{1, 2, 3, 0, 4, 13}))));
+        Assertions.assertTrue((new Polynomial(new int[]{1, 2, 3, 0, 4, 13})).differentiate(1).isEqual(new Polynomial((new int[]{2, 6, 0, 16, 65}))));
+        Assertions.assertTrue((new Polynomial(new int[]{1, 2, 3, 0, 4, 13})).differentiate(2).isEqual(new Polynomial((new int[]{6, 0, 48, 260}))));
+        Assertions.assertTrue((new Polynomial(new int[]{1, 2, 3, 0, 4, 13})).differentiate(10).isEqual(new Polynomial(new int[]{0})));
     }
 
     @Test
     void isEqual() {
-        Assertions.assertEquals("13x^5 + 4x^4 + 3x^2 + 2x + 1", (new Polynomial(new int[]{1, 2, 3, 0, 4, 13})).toString());
-        Assertions.assertEquals("5x^4 + 3x^2", (new Polynomial(new int[]{0, 0, 3, 0, 5})).toString());
+        Assertions.assertTrue( (new Polynomial(new int[]{1, 2, 3, 0, 4, 13})).isEqual(new Polynomial(new int[]{1, 2, 3, 0, 4, 13})));
+        Assertions.assertFalse( (new Polynomial(new int[]{1, 2, 3, 0, 4, 12})).isEqual(new Polynomial(new int[]{1, 2, 3, 0, 4, 13})));
     }
 
     @Test
