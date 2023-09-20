b76ad7320d4b6e2f8b1cfae32b23396a01df49e3
ZuZKho <matveymag@mail.ru>
2023-09-20 17:48:18 +0700

    check 1

--- Task_1_1_2/src/main/java/Polynomial.java
+++ Task_1_1_2/src/main/java/Polynomial.java
@@ -4,21 +4,29 @@
 public class Polynomial {
 
     private int[] coefs;
-    private int n;
+    private int len;
 
     Polynomial(int[] arr) {
-        this.n = arr.length;
+        this.len = arr.length;
         this.coefs = new int[arr.length];
-        System.arraycopy(arr, 0, this.coefs, 0, n);
+        System.arraycopy(arr, 0, this.coefs, 0, this.len);
     }
 
+    /**
+     * Данная функция удаляет ведущие нулевые коэффициенты.
+     *
+     * @param arr полином.
+     * @return полином без ведущих нулей.
+     */
     private int[] normalize(int[] arr) {
-        int nwN = Math.max(0, arr.length - 1);
-        while (nwN > 0 && arr[nwN] == 0) {
-            nwN--;
+        int newLen = Math.max(0, arr.length - 1);
+        while (newLen > 0 && arr[newLen] == 0) {
+            newLen--;
         }
-        int[] buf = new int[nwN + 1];
-        System.arraycopy(arr, 0, buf, 0, nwN + 1);
+
+        newLen++;
+        int[] buf = new int[newLen];
+        System.arraycopy(arr, 0, buf, 0, newLen);
         return buf;
     }
 
@@ -29,20 +37,19 @@ public class Polynomial {
      * @return результат сложения
      */
     public Polynomial plus(Polynomial b) {
-        int nwN = Math.max(this.n, b.n);
-        int[] newCoefs = new int[nwN];
+        int newLen = Math.max(this.len, b.len);
+        int[] newCoefs = new int[newLen];
 
-        for (int i = 0; i < nwN; i++) {
-            if (i < this.n) {
+        for (int i = 0; i < newLen; i++) {
+            if (i < this.len) {
                 newCoefs[i] += this.coefs[i];
             }
-            if (i < b.n) {
+            if (i < b.len) {
                 newCoefs[i] += b.coefs[i];
             }
         }
 
-        newCoefs = normalize(newCoefs);
-        return new Polynomial(newCoefs);
+        return new Polynomial(normalize(newCoefs));
     }
 
     /**
@@ -52,20 +59,19 @@ public class Polynomial {
      * @return разность полиномов
      */
     public Polynomial minus(Polynomial b) {
-        int nwN = Math.max(this.n, b.n);
-        int[] newCoefs = new int[nwN];
+        int newLen = Math.max(this.len, b.len);
+        int[] newCoefs = new int[newLen];
 
-        for (int i = 0; i < nwN; i++) {
-            if (i < this.n) {
+        for (int i = 0; i < newLen; i++) {
+            if (i < this.len) {
                 newCoefs[i] += this.coefs[i];
             }
-            if (i < b.n) {
+            if (i < b.len) {
                 newCoefs[i] -= b.coefs[i];
             }
         }
 
-        newCoefs = normalize(newCoefs);
-        return new Polynomial(newCoefs);
+        return new Polynomial(normalize(newCoefs));
     }
 
     /**
@@ -75,15 +81,14 @@ public class Polynomial {
      * @return произведение полиномов
      */
     public Polynomial times(Polynomial b) {
-        int nwN = this.n + b.n - 1;
-        int[] newCoefs = new int[nwN];
-        for (int i = 0; i < this.n; i++) {
-            for (int j = 0; j < b.n; j++) {
+        int newLen = this.len + b.len - 1;
+        int[] newCoefs = new int[newLen];
+        for (int i = 0; i < this.len; i++) {
+            for (int j = 0; j < b.len; j++) {
                 newCoefs[i + j] += this.coefs[i] * b.coefs[j];
             }
         }
-        newCoefs = normalize(newCoefs);
-        return new Polynomial(newCoefs);
+        return new Polynomial(normalize(newCoefs));
     }
 
     /**
@@ -109,29 +114,20 @@ public class Polynomial {
      * @return x-тая производная полинома
      */
     public Polynomial differentiate(int x) {
+        int[] newCoefs = new int[this.len];
+        int newLen = this.len;
+        System.arraycopy(this.coefs, 0, newCoefs, 0, this.len);
 
-        int[] newCoefs = new int[this.n];
-        int nn = this.n;
-        System.arraycopy(this.coefs, 0, newCoefs, 0, this.n);
-
-        for (int i = 0; i < Math.min(this.n, x); i++) {
-            System.arraycopy(newCoefs, 1, newCoefs, 0, nn - 1);
-            newCoefs[nn - 1] = 0;
-            for (int j = 0; j < nn - 1; j++) {
+        for (int i = 0; i < Math.min(this.len, x); i++) {
+            System.arraycopy(newCoefs, 1, newCoefs, 0, newLen - 1);
+            newCoefs[newLen - 1] = 0;
+            for (int j = 0; j < newLen - 1; j++) {
                 newCoefs[j] *= (j + 1);
             }
-            nn--;
+            newLen--;
         }
 
-        // Удалим лишние нули
-        int nwN = Math.max(0, nn - 1);
-        while (nwN > 0 && newCoefs[nwN] == 0) {
-            nwN--;
-        }
-        int[] buf = new int[nwN + 1];
-        System.arraycopy(newCoefs, 0, buf, 0, nwN + 1);
-
-        return new Polynomial(buf);
+        return new Polynomial(normalize(newCoefs));
     }
 
     /**
@@ -141,8 +137,8 @@ public class Polynomial {
      * @return равны ли полиномы
      */
     public boolean isEqual(Polynomial b) {
-        if (this.n != b.n) return false;
-        for (int i = 0; i < this.n; i++) {
+        if (this.len != b.len) return false;
+        for (int i = 0; i < this.len; i++) {
             if (this.coefs[i] != b.coefs[i]) {
                 return false;
             }
@@ -156,13 +152,12 @@ public class Polynomial {
      * @return строковое представление полинома
      */
     public String toString() {
-
-        if (this.n == 1 && this.coefs[0] == 0) {
+        if (this.len == 1 && this.coefs[0] == 0) {
             return "0";
         }
 
         String res = "";
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = this.len - 1; i >= 0; i--) {
             if (this.coefs[i] != 0) {
                 if (res == "") {
                     res += this.coefs[i];
@@ -178,6 +173,7 @@ public class Polynomial {
                 }
             }
         }
+
         return res;
     }
 
