9c93785df78329086da72ba2681ff2598cafb81a
ZuZKho <ZuZKho@users.noreply.github.com>
2024-02-08 18:05:41 +0000

    Deploying to gh-pages from @ ZuZKho/OOP@c041aab07da6fc593266a0d9899c837a2ea03d7d 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Multithread"},{"p":"<Unnamed>","l":"ParallelStream"},{"p":"<Unnamed>","l":"PrimeDetector"},{"p":"<Unnamed>","l":"Sequential"}]
